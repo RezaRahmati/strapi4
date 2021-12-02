766eaf49edbab387248bc248c882c1663b082ff9
Reza <Reza.Rahmati@gmail.com>
2021-12-01 21:34:09 -0500

    update db

--- config/database.js
+++ config/database.js
@@ -1,22 +1,16 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-
     if (env('NODE_ENV') === 'production') {
         return {
-            defaultConnection: 'default',
-            connections: {
-                default: {
-                    connector: 'bookshelf',
-                    settings: {
-                        client: 'postgres',
-                        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
-                        database: env('DATABASE_NAME'),
-                        username: env('DATABASE_USERNAME'),
-                        password: env('DATABASE_PASSWORD'),
-                    },
-                    options: {},
-                },
+            connection: {
+                client: 'postgres',
+                connection: {
+                    socketPath: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+                    database: env('DATABASE_NAME'),
+                    username: env('DATABASE_USERNAME'),
+                    password: env('DATABASE_PASSWORD'),
+                }
             },
         }
     } else {
